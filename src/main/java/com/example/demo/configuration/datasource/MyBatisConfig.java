0b0cc1f9dcf66e9da22520cd2907a289fd76c91c
dudumiaoer <1436607900@qq.com>
2019-11-08 14:54:07 +0800

    six

--- src/main/java/com/example/demo/configuration/datasource/MyBatisConfig.java
+++ src/main/java/com/example/demo/configuration/datasource/MyBatisConfig.java
@@ -2,6 +2,7 @@ package com.example.demo.configuration.datasource;
 
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
@@ -26,10 +27,18 @@ public class MyBatisConfig {
     @Resource(name = "myRoutingDataSource")
     private DataSource myRoutingDataSource;
 
+//    为了做数据源，所以将配置抽出到配置类中了，而关于驼峰转换的配置内容没有放进去，需要再次配置
     @Bean
-    public SqlSessionFactory sqlSessionFactory() throws Exception {
+    @ConfigurationProperties(prefix = "mybatis.configuration")
+    public org.apache.ibatis.session.Configuration globalConfiguration() {
+        return new org.apache.ibatis.session.Configuration();
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(org.apache.ibatis.session.Configuration config) throws Exception {
         SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
         sqlSessionFactoryBean.setDataSource(myRoutingDataSource);
+        sqlSessionFactoryBean.setConfiguration(config);
 //        sqlSessionFactoryBean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:mapper/*.xml"));
         return sqlSessionFactoryBean.getObject();
     }
